02e2bbf9ae2f044f7f5f1f06bd72cc849ff71a6f
CurrySoup <vo.chris.k@gmail.com>
2020-03-10 18:22:45 -0700

    Add my solution for palindrome

---
+++ 2-6-palindrome.js
@@ -0,0 +1,43 @@
+const Node = require("./node");
+
+/*
+    Cracking the Coding Interview - Chapter 2,
+
+    Assume that we have the length of the list.
+*/
+
+var counter = 1;
+
+const store = {
+    node: null,
+    flag: true
+}
+
+const checkPalindrome = function(head, length) {
+    if(counter === Math.round(length / 2)) {
+        if(head.data !== head.next.data && length % 2 === 0) store.flag = false;
+        
+        if(length % 2 === 0) {
+            store.node = head.next.next;
+        } else {
+            store.node = head.next;
+        }
+        return store;
+    }
+
+    counter++;
+    let newStore = checkPalindrome(head.next, length);
+    if (head.data !== newStore.node.data) {
+        store.flag = false;
+    }
+    newStore.node = newStore.node.next;
+    return newStore;
+}
+
+let node = new Node(2);
+node.append(1);
+node.append(2);
+node.append(1);
+node.append(2);
+
+console.log(checkPalindrome(node, 5).flag);
