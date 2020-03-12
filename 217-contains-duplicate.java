1ace00ffd6e56d4ed3840aa8f905e1adee71081a
CurrySoup <vo.chris.k@gmail.com>
2020-03-12 13:33:05 -0700

    Add my solution for contains duplicate

---
+++ 217-contains-duplicate.java
@@ -0,0 +1,19 @@
+/**
+ * Leetcode #217 - https://leetcode.com/problems/contains-duplicate/
+ */
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+
+        return false;
+    }
+}
