7a5c0fc97600dd8e852ecaa508b97daa26007d5b
CurrySoup <vo.chris.k@gmail.com>
2020-03-26 16:30:19 -0700

    Add my solution for validate bst

--- CTCI/4-5-validate-bst.js
+++ CTCI/4-5-validate-bst.js
@@ -1,23 +1,49 @@
 const BinaryTree = require("../Others/binary_tree");
 
 const validateBST = function(root) {
-    return validateSubTree(root.left, Number.POSITIVE_INFINITY, root.val) && validateSubTree(root.right, root.val, Number.NEGATIVE_INFINITY);
+    return validateLeftMost(root.left, Number.POSITIVE_INFINITY, root.data) && validateRightMost(root.right, root.data, Number.NEGATIVE_INFINITY);
 }
 
-const validateSubTree = function(root, min, max) {
+const validateLeftMost = function(root, min, max) {
     if(root === null) return true;
-
-    if(root.val <= min) {
-        min = root.val;
-    } else if(root.val > max) {
-        max = root.val;
-    } else if (root.val > min && root.val < max) {
-        return validateSubTree(root.left, min, max) && validateSubTree(root.right, min, max);
+    
+    if(root.data <= min) {
+        min = root.data;
+        return validateLeftMost(root.left, min, max) && validateRightSubTree(root.right, min, max);
     } else {
         return false;
     }
+}
 
-    return validateSubTree(root.left, min, max) && validateSubTree(root.right, min, max);
+const validateRightMost = function(root, min, max) {
+    if (root === null) return true;
+
+    if (root.data > max) {
+        max = root.data;
+        return validateRightMost(root.right, min, max) && validateLeftSubTree(root.left, min, max);
+    } else {
+        return false;
+    }
+}
+
+const validateRightSubTree = function(root, min, max) {
+    if(root === null) return true;
+
+    if(root.data >= min && root.data < max) {
+        return validateLeftMost(root.left, min, max) && validateRightSubTree(root.right, min, max);
+    } else {
+        return false;
+    }
+}
+
+const validateLeftSubTree = function (root, min, max) {
+    if (root === null) return true;
+
+    if (root.data >= min && root.data < max) {
+        return validateRightMost(root.right, min, max) && validateLeftSubTree(root.left, min, max);
+    } else {
+        return false;
+    }
 }
 
 const binaryTree = new BinaryTree(4);
@@ -30,11 +56,8 @@ binaryTree.append(7);
 
 console.log(validateBST(binaryTree.root));
 
-const anotherBinaryTree = new BinaryTree(4);
-anotherBinaryTree.append(5);
+const anotherBinaryTree = new BinaryTree(2);
 anotherBinaryTree.append(1);
-anotherBinaryTree.append(2);
 anotherBinaryTree.append(3);
-anotherBinaryTree.append(4);
 
 console.log(validateBST(anotherBinaryTree.root));
