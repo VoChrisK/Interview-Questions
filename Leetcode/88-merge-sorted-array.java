3206bdfa9c85dd626893a4010d187e4e26a5b59a
CurrySoup <vo.chris.k@gmail.com>
2020-06-13 17:43:45 -0700

    Add my solution for merge sorted array

---
+++ Leetcode/88-merge-sorted-array.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Leetcode #88 - https://leetcode.com/problems/merge-sorted-array/
+ */
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (m == 0) {
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+
+        for (int i = nums1.length - 1; i >= 0 && n > 0; i--) {
+            if (m > 0 && nums1[m - 1] > nums2[n - 1]) {
+                nums1[i] = nums1[m - 1];
+                m--;
+            } else {
+                nums1[i] = nums2[n - 1];
+                n--;
+            }
+        }
+    }
+}
